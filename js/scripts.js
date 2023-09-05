1d845ff847a3bdba438c354b4d6bfec1677fd05b
CodeMPok <codemp.ok@gmail.com>
2023-09-05 12:09:15 -0300

    proyecto qr remeras

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*function simulateProgress() {
+    let progressBar = document.getElementById("progress");
+    let width = 0
+    let interval = setInterval(function () {
+        if (width >= 100) {
+            clearInterval(interval);
+        } else {
+            width++;
+            progressBar.style.width = width + "%";
+        }
+    }, 10);
+}*/
